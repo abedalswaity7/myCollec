41ca46f25385cf801d3855f1c2a79b6048df25c0
Abdallah Ishaq Abdallah Alswaiti <abdallah@localhost.localdomain>
2022-05-24 16:09:59 +0000

    Iterate over a list of checks and messages to avoid code duplication

---
+++ WEEK3/all_checks.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import sys
+
+def check_reboot():
+	"""Returns True if the computer has a pending reboot."""
+	return os.path.exists("/run/reboot-required")
+
+def check_disk_full(disk, min_gb, min_percent):
+	"""Returns True if there isn't enough disk space, False otherwise."""
+	du = shutill.disk_usage(disk)
+	#calculate the percentage of free space 
+	percent_free = 100* du.free /du.total
+	#calculate how many free gigabytes\
+	gigabytes_free =du.free/2**30
+	if gigabytes_free < min_gb or percent_free < min_percent:
+		return True
+	return False
+
+def check_root_full():
+	"""Returns True if the root partition is full, False otherwise"""
+	return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+def main():
+	checks=[
+	(check_reboot, "Pending Reboot"),
+	(check_root_full, "Root partition full")]
+
+	for check, msg in checks:
+		if check():
+			print(msg)
+			sys.exit(1)
+			
+	# if check_reboot():
+	# 	print("Pending Reboot")
+	# 	sys.exit(1)
+	# if check_root_full(disk="/",min_gb=2 , min_percent=10):
+	# 	print("Root partition full.")
+	# 	sys.exit(1)
+
+	print("Everything ok.")
+	sys.exit(0)
